37916d06215b3ec2d98cc539ab763566e24c9fa2
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2025-04-06 21:19:49 +0200

    green egypt project initialized.

---
+++ green_egypt/src/main/java/tech/amg/green_egypt/GreenEgyptApplication.java
@@ -0,0 +1,13 @@
+package tech.amg.green_egypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenEgyptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenEgyptApplication.class, args);
+    }
+
+}
