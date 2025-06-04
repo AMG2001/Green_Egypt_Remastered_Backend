3f5eebbfa3cdbf05a2710f95570d3433536ca9ad
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2025-06-05 00:54:39 +0200

    implement login and register options.

--- green_egypt/src/main/java/tech/amg/green_egypt/domain/enums/UserType.java
+++ green_egypt/src/main/java/tech/amg/green_egypt/domain/enums/UserType.java
@@ -1,5 +1,5 @@
 package tech.amg.green_egypt.domain.enums;
 
 public enum UserType {
-    ADMIN,CUSTOMER;
+    ADMIN,USER
 }
--- green_egypt/src/main/java/tech/amg/green_egypt/domain/model/User.java
+++ green_egypt/src/main/java/tech/amg/green_egypt/domain/model/User.java
@@ -4,26 +4,34 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.stereotype.Component;
+
+import org.springframework.data.mongodb.core.mapping.Document;
 import tech.amg.green_egypt.domain.enums.UserType;
 
 @Data
-public class RegisteredUser{
+@Document(collection = "users")
+public class User {
+    
+    @Email
+    @NotNull
+    private String email;
+    
     @NotNull
     private String firstName;
     @NotNull
     private String lastName;
-    @Email
-    @NotNull
-    private String email;
     @NotNull
     private String password;
     @NotNull
     private String mobileNumber;
     @NotNull
-    @JsonProperty("userType")  // Make sure Jackson knows the property name
+    @JsonProperty("userType")
     private UserType userType;
 
-    public RegisteredUser() {
+    private String createdAt;
+    private String updatedAt;
+
+    public User() {
     }
+
 }
