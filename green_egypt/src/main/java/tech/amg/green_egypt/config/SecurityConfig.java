aaeebf8cd9e6a0f6ac10aabb6383e7ce56e7518b
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2025-06-21 19:23:26 +0200

    Implement security and CORS config.

--- green_egypt/src/main/java/tech/amg/green_egypt/config/SecurityConfig.java
+++ green_egypt/src/main/java/tech/amg/green_egypt/config/SecurityConfig.java
@@ -3,61 +3,67 @@ package tech.amg.green_egypt.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @Slf4j
 public class SecurityConfig {
 
-    private List<String> authenticatedEndpoints = new ArrayList<>();
     private List<String> nonAuthenticatedEndpoints = new ArrayList<>();
     private final ControllersConfig controllersConfig;
-
+    // 3021 - etisalat
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        log.info("Configuring security with non-auth endpoints");
-        
         http
                 .authorizeHttpRequests(requests ->
                         requests
-                        // Note that request Matcher is working on the path after context path.        
-                        .requestMatchers(convertStringListToStringArray(nonAuthenticatedEndpoints)).permitAll()
-                                .anyRequest().authenticated()
+                                .requestMatchers(convertStringListToStringArray(nonAuthenticatedEndpoints))
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults()); // Enable CORS
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+                .cors(cors -> cors.configurationSource(
+                        request -> buildCorsConfiguration()
+                        )
+                );
         return http.build();
     }
 
     public SecurityConfig(ControllersConfig controllersConfig) {
         this.controllersConfig = controllersConfig;
-        // define all authenticated and non authenticated endpoints.
         initiateRequestMatcherControllers();
     }
 
     private void initiateRequestMatcherControllers() {
-        // define all non authenticated endpoints
+        // define all non authenticated endpoints with the correct context path
+        String baseUrl = controllersConfig.getBaseUrl();
         nonAuthenticatedEndpoints.add(controllersConfig.getRegisterPageUrl());
         nonAuthenticatedEndpoints.add(controllersConfig.getLoginPageUrl());
-
-        // define all authenticated endpoints.
     }
 
-    /**
+    /*
      * Used in case the you want to convert List<String> to String[]
-     *
-     * @param list
-     * @return
      */
     private String[] convertStringListToStringArray(List<String> list) {
         return list.toArray(new String[list.size()]);
     }
+
+    private CorsConfiguration buildCorsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setMaxAge(3600L);
+        return corsConfiguration;
+    }
+
 }
